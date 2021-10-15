86c1a615d5e29857bc909f826d5c9abd8bcd810e
ipk1 <32953048+ipk1@users.noreply.github.com>
2021-10-15 22:41:19 +0530

    Update Synacksubs.sh

--- Synacksubs.sh
+++ Synacksubs.sh
@@ -7,7 +7,10 @@ curl 'https://platform.synack.com/api/targets/registered_summary'   -H 'Connecti
 paste subid.txt org.txt | while read m k
 do
 
-curl 'https://platform.synack.com/api/targets/'$m''   -H 'authority: platform.synack.com'   -H 'authorization: Bearer '$1''  --compressed | jq '.codename' >> subdomains.txt
-curl 'https://platform.synack.com/api/targets/'$m'/vulnerability_category_exploitable_locations'  -H 'Connection: keep-alive'  -H 'Authorization: Bearer '$1''   --compressed | jq -r '.[] | .ExploitableLocations[] | .value' | cut -d '/' -f 3 | grep -v -e ' ' -e '>' | sort -u >> subdomains.txt
+#curl 'https://platform.synack.com/api/targets/'$m''   -H 'authority: platform.synack.com'   -H 'authorization: Bearer '$1''  --compressed | jq '.codename' >> subdomains.txt
+#curl 'https://platform.synack.com/api/targets/'$m'/vulnerability_category_exploitable_locations'  -H 'Connection: keep-alive'  -H 'Authorization: Bearer '$1''   --compressed | jq -r '.[] | .ExploitableLocations[] | .value' | cut -d '/' -f 3 | grep -v -e ' ' -e '>' | sort -u >> subdomains.txt
 
+curl 'https://platform.synack.com/api/targets/'$m''   -H 'authority: platform.synack.com'   -H 'authorization: Bearer '$1''  --compressed | jq '.codename' | sudo tee subdomains.txt fullpath.txt
+curl 'https://platform.synack.com/api/listing_analytics/categories?listing_id='$m'&status=accepted' -H 'Connection: keep-alive'  -H 'Authorization: Bearer '$1''   --compressed |  jq -r '.value[] | .exploitable_locations[] | .value' | cut -d '/' -f 3 | grep -v -e ' ' -e '>' | sort -u >> subdomains.txt
+curl 'https://platform.synack.com/api/listing_analytics/categories?listing_id='$m'&status=accepted' -H 'Connection: keep-alive'  -H 'Authorization: Bearer '$1''   --compressed |  jq -r '.value[] | .exploitable_locations[] | .value' >> fullpath.txt
 done
